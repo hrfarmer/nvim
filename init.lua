c4d8859091157d9a045bb5186a1d68499c6e0299
aychar <58487401+hrfarmer@users.noreply.github.com>
2025-04-13 22:11:16 -0500

    add light/dark theme switching and saving

--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 require("aychar")
 require("aychar.lazy")
 
-vim.cmd("colorscheme github_light_default")
-vim.cmd("hi Normal ctermbg=none guibg=none")
 vim.o.number = true
 vim.o.relativenumber = true
 
@@ -23,3 +21,48 @@ autocmd("FileType", {
 
 vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
+
+function InitializeThemeFile()
+	local file = io.open(vim.fn.stdpath("config") .. "/.theme", "w")
+	if file then
+		file:write("dark\n")
+		file:close()
+	end
+
+	vim.cmd("colorscheme github_dark_default")
+end
+
+local themeFile = io.open(vim.fn.stdpath("config") .. "/.theme", "r")
+if themeFile then
+	local theme = themeFile:read("*l")
+	themeFile:close()
+
+	if theme and #theme > 0 then
+		if theme == "light" then
+			vim.cmd("colorscheme github_light_default")
+		else
+			vim.cmd("colorscheme github_dark_default")
+		end
+	else
+		InitializeThemeFile()
+	end
+else
+	InitializeThemeFile()
+end
+
+vim.api.nvim_create_user_command("Theme", function(opts)
+	local file = io.open(vim.fn.stdpath("config") .. "/.theme", "w")
+	if file then
+		if opts.fargs[1] == "light" then
+			vim.cmd("colorscheme github_light_default")
+			file:write("light\n")
+		elseif opts.fargs[1] == "dark" then
+			vim.cmd("colorscheme github_dark_default")
+			file:write("dark\n")
+		end
+		vim.cmd("hi Normal ctermbg=none guibg=none")
+		file:close()
+	end
+end, { nargs = 1 })
+
+vim.cmd("hi Normal ctermbg=none guibg=none")
