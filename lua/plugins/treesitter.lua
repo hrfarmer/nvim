114b77ea8d50eea7e8a63578d9016d0956a63882
aychar <aychar@aychar.dev>
2026-04-05 18:31:21 -0500

    config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function ()
-		local configs = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = { "c", "cpp", "rust", "lua", "javascript", "typescript", "astro", "css", "html" },
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end
+        configs.setup({
+            ensure_installed = { "c", "cpp", "rust", "lua", "javascript", "typescript", "astro", "css", "html", "zig" },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
 }
